f19aa4ef3daf972debbda075158ecfe336cfb0a8
Joseph <Jpatrick148@Gmail.com>
2018-11-16 14:05:58 -0500

    First Commit

---
+++ ListWebApp/Models/Info.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ListWebApp.Models
+{
+    public class Info
+    {
+        [Key] 
+        public int ID { get; set; }
+
+    }
+
+    public class User : Info
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Task : Info
+    {
+        public string Description { get; set; }
+        public DateTime DueDateTime { get; set; }
+        public bool IsComplete = false;
+    }
+
+    public class InformationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
